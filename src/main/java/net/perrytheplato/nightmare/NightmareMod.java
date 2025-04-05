b7f9e7df0546c12acf0edd684756d0c922ecf423
perry <sanjithramesh2020@gmail.com>
2025-04-04 19:15:52 -0700

    0.0.2v

---
+++ src/main/java/net/perrytheplato/nightmare/NightmareMod.java
@@ -0,0 +1,20 @@
+package net.perrytheplato.nightmare;
+
+import net.fabricmc.api.ModInitializer;
+import net.perrytheplato.nightmare.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NightmareMod implements ModInitializer {
+	public static final String MOD_ID = "nightmare-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerModItems();
+	}
+
+	public static String getModId() {
+		return MOD_ID;
+	}
+}
---
+++ src/main/java/net/perrytheplato/nightmare/NightmareModClient.java
@@ -0,0 +1,10 @@
+package net.perrytheplato.nightmare;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NightmareModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
