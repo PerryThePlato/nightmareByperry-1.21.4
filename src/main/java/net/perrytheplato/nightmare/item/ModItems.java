559112d41e2d163b0fb05b756ee296da8ca7cf29
perry <sanjithramesh2020@gmail.com>
2025-04-04 19:14:37 -0700

    0.0.2v

--- src/main/java/net/perrytheplato/nightmare/item/ModItems.java
+++ src/main/java/net/perrytheplato/nightmare/item/ModItems.java
@@ -1,43 +1,30 @@
 package net.perrytheplato.nightmare.item;
 
-
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.perrytheplato.nightmare.NightmareMod;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-
+import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item MAGNETITE = registerItem("magnetite", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NightmareMod.MOD_ID,"magnetite")))));
+    public static final Item RAW_MAGNETITE = registerItem("raw_magnetite", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NightmareMod.MOD_ID,"raw_magnetite")))));
 
-    public static final Item MAGNETITE = registerItem(new Item.Settings());
 
-
-
-    private static Item registerItem(Item.Settings itemSettings) {
-        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NightmareMod.MOD_ID, "magnetite"));
-        Item item = new Item(itemSettings.registryKey(key));
-        return Registry.register(Registries.ITEM, key, item);
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(NightmareMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
-
         NightmareMod.LOGGER.info("Registering Mod Items for " + NightmareMod.MOD_ID);
 
-
-
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-
             entries.add(MAGNETITE);
-
+            entries.add(RAW_MAGNETITE);
         });
-
     }
-
-
 }
